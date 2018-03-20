f316d7bf70031f3d6400194569290619b4e43d5f
PedroMarthon <pedro_marthon@live.com>
2018-03-20 13:53:10 +0200

    added bootstrap to shopping

--- php_test_lab/index.php
+++ php_test_lab/index.php
@@ -1,28 +1,60 @@
 <!DOCTYPE html>
-<?php
-    session_start();
-    include 'encryptor.php';
-?>
+
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Main Page Test</title>
+    <meta http-equiv="cache-control" content="max-age=0" />
+    <meta http-equiv="cache-control" content="no-cache" />
+    <meta http-equiv="pragma" content="no-cache" />
+
+    <title>Mawla Telecom Shop</title>
+
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/mystyle.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="js/bootstrap.js"></script>
 </head>
+
 <body>
-    <header>
-        <h1>MAWLA TELECOM</h1>
-        <?php
-            if(isset($_COOKIE['loggedEmail'])){
-                $_SESSION['theNameLoggedIn'] = $_COOKIE['loggedName'];
-                $_SESSION['theEmailLogin'] = $_COOKIE['loggedEmail'];
-                echo "Logged as: " . doDecrypt($_SESSION['theNameLoggedIn']) . '<br><a href="logout-exec.php">Logout</a>';
-            }
-            else{
-                echo '<a href="login.php">Login</a>';
-            }
-        ?>
-    </header>
+    <!-- Top Navbar start -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <a href="index.php" class="navbar-brand">Mawla Telecom</a>
+            <ul class="nav navbar-nav">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Phones <span class="caret"></span></a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="#">Samsung</a></li>
+                        <li>
+                            <a href="#">Huawei</a>
+                        </li>
+                        <li>
+                            <a href="#">Apple</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <!-- Top Navbar end -->
+    <!-- Header start -->
+    <div id="theHeader">
+        <!-- SlideShow here -->
+    </div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita nostrum natus recusandae mollitia quod, placeat blanditiis sunt nesciunt totam earum corrupti in eaque ut at rem. Beatae amet quasi inventore. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Itaque sequi, dolore cum accusamus dicta magnam, minus et sed officia asperiores exercitationem tempora ullam eligendi. Quibusdam minima quaerat id aliquam corrupti! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
+        ea quos ex quibusdam assumenda obcaecati atque dicta omnis ut provident numquam reprehenderit, accusantium quae. Dicta fugit dolorem possimus placeat blanditiis. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos rerum aut nihil possimus
+        molestiae, error suscipit deleniti eum repellat vero totam temporibus aliquid architecto accusantium iste cum, modi ad minus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, non. Laborum excepturi, enim incidunt vero, unde
+        adipisci sed esse corrupti laboriosam vel, placeat voluptatibus quia facilis officiis. Quidem, nulla aperiam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita pariatur sapiente obcaecati earum, quam itaque placeat eveniet harum
+        reprehenderit cumque nisi minima odit amet magnam, velit consequatur sed fuga? Aperiam. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum optio quae doloremque tenetur nulla enim dolore at, nobis amet, saepe repellendus debitis tempora
+        et quibusdam! Animi quasi iste velit nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, in quibusdam? Vitae, veniam. Debitis veritatis a ex dolorem cupiditate reiciendis quidem eius nisi similique modi, quibusdam fuga vel
+        dolores voluptas.
+    </p>
+
+
 </body>
+
 </html>
