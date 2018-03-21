f359875dee54d0b33332dfc2b5789e8666cf3545
PedroMarthon <pedro_marthon@live.com>
2018-03-21 14:28:17 +0200

    upgraded shopping site to bootstrap 4, working on navbar in shopping page

--- php_test_lab/index.php
+++ php_test_lab/index.php
@@ -12,32 +12,54 @@
 
     <title>Mawla Telecom Shop</title>
 
-    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/mystyle.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="js/bootstrap.js"></script>
+    <script src="js/bootstrap.min.js"></script>
 </head>
 
 <body>
     <!-- Top Navbar start -->
-    <nav class="navbar navbar-default navbar-fixed-top">
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light">
         <div class="container">
-            <a href="index.php" class="navbar-brand">Mawla Telecom</a>
-            <ul class="nav navbar-nav">
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Phones <span class="caret"></span></a>
-                    <ul class="dropdown-menu" role="menu">
-                        <li><a href="#">Samsung</a></li>
-                        <li>
-                            <a href="#">Huawei</a>
-                        </li>
-                        <li>
-                            <a href="#">Apple</a>
-                        </li>
-                    </ul>
-                </li>
-            </ul>
+            <a class="navbar-brand" href="#">MAWLA TELECOM</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Devices
+                            </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Samsung</a>
+                            <a class="dropdown-item" href="#">Huawei</a>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Cards
+                            </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Touch</a>
+                        </div>
+                    </li>
+                </ul>
+                <span class="navbar-text">
+                        Welcome: $USER
+                    </span>
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </div>
         </div>
+
     </nav>
     <!-- Top Navbar end -->
     <!-- Header start -->
@@ -51,7 +73,21 @@
         adipisci sed esse corrupti laboriosam vel, placeat voluptatibus quia facilis officiis. Quidem, nulla aperiam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita pariatur sapiente obcaecati earum, quam itaque placeat eveniet harum
         reprehenderit cumque nisi minima odit amet magnam, velit consequatur sed fuga? Aperiam. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum optio quae doloremque tenetur nulla enim dolore at, nobis amet, saepe repellendus debitis tempora
         et quibusdam! Animi quasi iste velit nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, in quibusdam? Vitae, veniam. Debitis veritatis a ex dolorem cupiditate reiciendis quidem eius nisi similique modi, quibusdam fuga vel
-        dolores voluptas.
+        dolores voluptas. Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita nostrum natus recusandae mollitia quod, placeat blanditiis sunt nesciunt totam earum corrupti in eaque ut at rem. Beatae amet quasi inventore. Lorem ipsum dolor
+        sit amet consectetur adipisicing elit. Itaque sequi, dolore cum accusamus dicta magnam, minus et sed officia asperiores exercitationem tempora ullam eligendi. Quibusdam minima quaerat id aliquam corrupti! Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Quo ea quos ex quibusdam assumenda obcaecati atque dicta omnis ut provident numquam reprehenderit, accusantium quae. Dicta fugit dolorem possimus placeat blanditiis. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos
+        rerum aut nihil possimus molestiae, error suscipit deleniti eum repellat vero totam temporibus aliquid architecto accusantium iste cum, modi ad minus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, non. Laborum excepturi,
+        enim incidunt vero, unde adipisci sed esse corrupti laboriosam vel, placeat voluptatibus quia facilis officiis. Quidem, nulla aperiam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita pariatur sapiente obcaecati earum, quam itaque
+        placeat eveniet harum reprehenderit cumque nisi minima odit amet magnam, velit consequatur sed fuga? Aperiam. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum optio quae doloremque tenetur nulla enim dolore at, nobis amet, saepe repellendus
+        debitis tempora et quibusdam! Animi quasi iste velit nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, in quibusdam? Vitae, veniam. Debitis veritatis a ex dolorem cupiditate reiciendis quidem eius nisi similique modi, quibusdam
+        fuga vel dolores voluptas. Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita nostrum natus recusandae mollitia quod, placeat blanditiis sunt nesciunt totam earum corrupti in eaque ut at rem. Beatae amet quasi inventore. Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Itaque sequi, dolore cum accusamus dicta magnam, minus et sed officia asperiores exercitationem tempora ullam eligendi. Quibusdam minima quaerat id aliquam corrupti! Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Quo ea quos ex quibusdam assumenda obcaecati atque dicta omnis ut provident numquam reprehenderit, accusantium quae. Dicta fugit dolorem possimus placeat blanditiis. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos
+        rerum aut nihil possimus molestiae, error suscipit deleniti eum repellat vero totam temporibus aliquid architecto accusantium iste cum, modi ad minus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, non. Laborum excepturi,
+        enim incidunt vero, unde adipisci sed esse corrupti laboriosam vel, placeat voluptatibus quia facilis officiis. Quidem, nulla aperiam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita pariatur sapiente obcaecati earum, quam itaque
+        placeat eveniet harum reprehenderit cumque nisi minima odit amet magnam, velit consequatur sed fuga? Aperiam. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum optio quae doloremque tenetur nulla enim dolore at, nobis amet, saepe repellendus
+        debitis tempora et quibusdam! Animi quasi iste velit nihil. Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, in quibusdam? Vitae, veniam. Debitis veritatis a ex dolorem cupiditate reiciendis quidem eius nisi similique modi, quibusdam
+        fuga vel dolores voluptas.
     </p>
 
 
