60963dc9db88905b01c22e4dc0151bd724663065
PedroMarthon <pedro_marthon@live.com>
2018-04-03 12:35:25 +0300

    fixed images

--- test_lab/loadingscreen_test.js
+++ test_lab/loadingscreen_test.js
@@ -4,15 +4,15 @@ document.body.style.overflow = 'hidden';
 
 //keeps calling until the page stops loading
 var loading = setInterval(function() {
-    
-    if(document.readyState === 'complete') {
-        
+
+    if (document.readyState === 'complete') {
+
         //revert scroller in the screen to its initial state
-        document.body.style.overflow = 'scroll';
         clearInterval(loading);
-        stoploadinginterval = setInterval(stoploadingf,10);
-    }else if(window.scrollY > 0){
-        window.scrollTo(0,0); //This will help display loading screen even if the initial scroll location is not zero
+        document.body.style.overflow = 'scroll';
+        stoploadinginterval = setInterval(stoploadingf, 10);
+    } else if (window.scrollY > 0) {
+        window.scrollTo(0, 0); //This will help display loading screen even if the initial scroll location is not zero
     }
 }, 100);
 
@@ -29,17 +29,16 @@ var stoploadinginterval;
 
 //Third Frame, it fades the object by decreasing opacity gradely(Not sure if it is an actual word :P)
 function stoploadingf() {
+
     //Somehow the opacity skips value = 0 so used less than comparison
-    if(objOpacity < 0){
+    if (objOpacity < 0) {
         //Since the opacity will skip 0, this will restore it and remove negative opacity...
         loadingscreen.style.opacity = 0;
         loadingscreen.style.display = 'none';
         //Stop the third animation
         clearInterval(stoploadinginterval);
-        
-    }
-    else
-    {
+
+    } else {
         objOpacity -= 0.01;
 
         //Tried some numbers and it turned out to be perfect with those, dont really know why XD
